84b223b2a64c2611b64f77da07dde9075ccb3c9e
Matheus Pereira de Almeida <matheus.pereira.almeida10@gmail.com>
2020-05-16 18:16:33 -0300

    First Commit

---
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/ProdutoDAOEntity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    internal class ProdutoDAOEntity : IDisposable, IProdutoDAO
+    {
+        private LojaContext contexto;
+
+        public ProdutoDAOEntity()
+        {
+            this.contexto = new LojaContext();
+        }
+
+        public void Adicionar(Produto p)
+        {
+            contexto.Produtos.Add(p);
+            contexto.SaveChanges();
+        }
+
+        public void Atualizar(Produto p)
+        {
+            contexto.Produtos.Update(p);
+            contexto.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+
+        public IList<Produto> Produtos()
+        {
+            return contexto.Produtos.ToList();
+            
+        }
+
+        public void Remover(Produto p)
+        {
+            contexto.Produtos.Remove(p);
+            contexto.SaveChanges();
+        }
+    }
+}
---
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GravarUsandoAdoNet();
+           // GravarUsandoEntity();
+           // RecuperaProdutos();
+           //ExcluirProdutos();
+            AtualizarProdutos();
+            Console.Read();
+        }
+
+        private static void AtualizarProdutos()
+        {
+            using (var repo = new ProdutoDAOEntity())
+            {
+                Produto produto = repo.Produtos().First();
+                produto.Nome = "Harry Potter e a Ordem da Fênix - Editado";
+                repo.Atualizar(produto);
+            }
+        }
+
+        private static void GravarUsandoAdoNet()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            using (var repo = new ProdutoDAOEntity())
+            {
+                repo.Adicionar(p);
+            }
+        }
+        private static void GravarUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            using (var contexto = new ProdutoDAOEntity())
+            {
+                contexto.Adicionar(p);
+            }
+        }
+
+        private static void RecuperaProdutos()
+        {
+            using (var repo = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = repo.Produtos().ToList();
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine(item.Nome);
+                }
+            }
+        }
+        private static void ExcluirProdutos()
+        {
+            using (var repo = new ProdutoDAOEntity())
+            {
+                IList<Produto> produtos = repo.Produtos().ToList();
+                foreach (var item in produtos)
+                {
+                    repo.Remover(item);
+                }
+            }
+        }
+    }
+}
